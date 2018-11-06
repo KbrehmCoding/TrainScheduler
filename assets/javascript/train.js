f825f04b71f9922991be9377ac9a1f4c934c24e9
Krista Brehm <kristabrehm1@gmail.com>
2018-11-06 18:26:34 -0500

    added bootstrap, and changed arround css to make it work with bootstrap, started adding in firebase and js

---
+++ assets/javascript/train.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.5.7/firebase.js"></script>
+<script>
+    // Initialize Firebase
+    var config = {
+    apiKey: "AIzaSyB2gLeq7KxSobVV8Iv5CYgHVf6EM_w-MNw",
+    authDomain: "classproject-ca27e.firebaseapp.com",
+    databaseURL: "https://classproject-ca27e.firebaseio.com",
+    projectId: "classproject-ca27e",
+    storageBucket: "classproject-ca27e.appspot.com",
+    messagingSenderId: "428460988398"
+    };
+    firebase.initializeApp(config);
+</script>
